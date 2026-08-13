ecf37bf136169b21bd7429a8cea85917368a36d1
telliGS <tellig270@gmail.com>
2026-08-12 22:55:13 -0300

    fix: rutas de imágenes con /images/ en Home y galería

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -485,8 +485,12 @@ export default function Home() {
               </div>
             </div>
             <div className="rounded-xl overflow-hidden border border-outline shadow-xl bg-surface-1 aspect-[4/3]">
-              <img src="/historia-1.jpg" alt="Equipo del Club José Hernández" className="w-full h-full object-cover" />
-            </div>
+  <img
+    src="/images/historia-1.jpg"
+    alt="Equipo del Club José Hernández"
+    className="w-full h-full object-cover"
+  />
+</div>
           </div>
         </section>
 
