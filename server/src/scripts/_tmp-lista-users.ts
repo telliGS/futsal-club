566d304bda73281d9e37dbf2a18dd4663cfb04d5
telliGS <tellig270@gmail.com>
2026-08-14 14:33:28 -0300

    chore: quitar script temporal de verificación

--- server/src/scripts/_tmp-lista-users.ts
+++
@@ -1,9 +0,0 @@
-import "dotenv/config";
-import { prisma } from "../config.js";
-
-const users = await prisma.user.findMany({
-  where: { role: "ADMIN" },
-  select: { email: true, fullName: true, role: true, teamAccess: { select: { teamId: true } } },
-});
-console.log(JSON.stringify(users, null, 2));
-await prisma.$disconnect();
